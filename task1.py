2af454553c34f4145a4ec45c755265242085ff67
Visible454 <99467435+Visible454@users.noreply.github.com>
2022-02-22 20:18:05 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,16 @@
+# importing the module
+import sys
+  
+# storing the arguments
+program = sys.argv[0]
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+  
+# displaying the program name
+print("Program name : " + program)
+  
+# displaying the arguments
+print("arg1 : " + arg1)
+print("arg2 : " + arg2)
+print("Number of arguments : ", len(sys.argv))
+print(sys.argv)
